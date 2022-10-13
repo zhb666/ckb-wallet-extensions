b6ca9e7a830a7660f5f544324460ddddb59c1fcc
zhb666 <462612421@qq.com>
2022-10-13 08:16:51 +0800

    feat: background.js

--- src/pages/components/Progress/index.tsx
+++ src/pages/components/Progress/index.tsx
@@ -23,7 +23,7 @@ function Progress() {
 
 		let tipHeaderNum = parseInt(tipHeaderRes.number)
 
-		// 没有匹配上钱包需要处理
+		// 没有匹配上钱包需要处理 
 		if (scriptsFilter && scriptsFilter[0]?.block_number) {
 			let scriptsNum = parseInt(scriptsFilter[0].block_number || 0)
 
